dec0c1bd1ce886e37ac24650689b29f7a58d20d4
TANHAPON TOSIRIKUL <tonydude1999@gmail.com>
2021-10-16 18:33:19 +0700

    added full functional eb

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,6 +1,11 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize(
-  `postgres://${config.username}:${config.password}@${config.host}:5432/${config.database}`
-);
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  dialect: "postgres",
+  storage: ":memory:",
+});
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://udagram-be-env.eba-pdcc4r63.us-west-2.elasticbeanstalk.com/api/v0",
 };
 
 /*
